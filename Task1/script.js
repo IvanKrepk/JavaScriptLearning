dd9738126aa0716a71d9d66c4c5e936277825f35
Ivan <i.am.krepkov.ivan@yandex.ru>
2025-02-18 14:58:00 +0300

    Corrected Task 1

--- Task1/script.js
+++ Task1/script.js
@@ -3,20 +3,21 @@
 let money = prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let required_expense_item;
-let required_expense_item_value;
-
 let appData = {
     budget: money,
-    expenses: {
-        required_expense_item: required_expense_item_value
-    },
+    timeData: time,
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
 
-required_expense_item = prompt("Введите обязательную статью расходов в этом месяце");
-required_expense_item_value = prompt("Во сколько обойдется?");
+let expenses_item_key_1 = prompt("Введите обязательную статью расходов в этом месяце");
+let expenses_item_value_1 = prompt("Во сколько обойдется?");
+let expenses_item_key_2 = prompt("Введите обязательную статью расходов в этом месяце");
+let expenses_item_value_2 = prompt("Во сколько обойдется?");
+
+appData.expenses.expenses_item_key_1 = expenses_item_value_1;
+appData.expenses.expenses_item_key_2 = expenses_item_value_2;
 
 alert("Бюджет на 1 день - " + appData.budget / 30);
